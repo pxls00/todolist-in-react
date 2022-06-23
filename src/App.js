04dc8a2d38e96e2a225c85e025ba53951ee0fd31
Abdurahim <abdurahimnasirov123@gmail.com>
2022-06-23 11:39:28 +0500

    Created components (Todolist <- TodoItem)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="wrapper">
       <h1 className='title-form'>Todo List</h1>
       <form>
-        ../
+        <TodoList />
       </form>
     </div>
   )
---
+++ src/components/TodoList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoItem from './TodoItem'
+
+export default function TodoList() {
+    return (
+        <ul>
+            <TodoItem />
+        </ul>
+    )
+}

621ba018f59458a0e19a0ec2f664978ac5954352
Abdurahim <abdurahimnasirov123@gmail.com>
2022-06-23 23:28:39 +0500

    There worked with Jsx and React.useState     Also changed some styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react'
 import TodoList from './components/TodoList';
 function App() {
-  let todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: 'Buy Bread'},
-    {id: 2, completed: false, title: 'Buy Pen'},
+    {id: 2, completed: true, title: 'Buy Pen'},
     {id: 3, completed: false, title: 'Buy Potatos'}
-  ]
+  ])
+  function todoToggle(id) {
+    setTodos(
+      todos.map(todo => {
+        if(todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        console.log(todo);
+        return todo
+      })
+    )
+  }
   return (
     <div className="wrapper">
       <h1 className='title-form'>Todo List</h1>
       <form>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} todoToggle={todoToggle}/>
       </form>
     </div>
   )
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function TodoItem({todo, index}) {
+function TodoItem({todo, index, todoToggle}) {
+    const classes = []
+    if (todo.completed) {
+        classes.push('done')
+    }
     return (
         <li className='todo-item'>
-           <strong>{++index}.</strong> {todo.title}
+            <span 
+                className={todo.completed ? 'todo-item__block done': 'todo-item__block'}
+            >
+                <input
+                    type="checkbox"
+                    className="todo-item__checkbox"
+                    checked={todo.completed}
+                    onChange={() => todoToggle(todo.id)}/>
+                <strong>{++index}.</strong> 
+                {todo.title}
+            </span>
+            <button className='todo-item__delete'>&times;</button>
         </li>
     )
 }
 TodoItem.prototype = {
     todo: PropTypes.object.isRequired,
-    index: PropTypes.number
+    index: PropTypes.number,
+    todoToggle: PropTypes.func.isRequired
 } 
 
 export default TodoItem
